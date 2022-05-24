8b0282df4fcf20cc38e5d4f654dc12bb67aff453
Mohsin Rashid <mohsinrashid32@gmail.com>
2022-05-24 19:15:43 +0500

    UI Update for Login screen

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -38,18 +38,19 @@ const Login = ({ service, setAuth, skipAuth }) => {
     }
 
     return (
-        <div>
-            <h2>{(service.header) ? service.header : 'Please Login'}</h2>
-            <p>{(service.description) ? service.description : ''}</p>
-            <span class>{failureMsg}</span>
-            <form>
-                <input value={username} onChange={setEmailValue} type="text" placeholder="Enter email" className="px-4 pt-4 pb-4 border w-full rounded-md" />
-                <input value={password} onChange={setPassowrdValue} type="password" placeholder="password" className="px-4 pt-4 pb-4 border w-full rounded-md" />
-                <button onClick={handleSkip}>Skip</button>
-                <button onClick={handleSubmit}>{(service.confirmLabel) ? service.confirmLabel : 'Login'}</button>
-            </form>
+        <div className='login-form-holder'>
+            <div className='login-form'>
+                <h2>{(service.header) ? service.header : 'Please Login'}</h2>
+                <p>{(service.description) ? service.description : ''}</p>
+                <span class>{failureMsg}</span>
+                <form>
+                    <div className='mb-5'><input value={username} onChange={setEmailValue} type="text" placeholder="Enter email" className="px-4 pt-4 pb-4 border w-full rounded-md" /></div>
+                    <div className='mb-5'><input value={password} onChange={setPassowrdValue} type="password" placeholder="password" className="px-4 pt-4 pb-4 border w-full rounded-md" /></div>
+                    <button className='login-button p-3' onClick={handleSubmit}>{(service.confirmLabel) ? service.confirmLabel : 'Login'}</button>
+                    <button className='skip-button p-3' onClick={handleSkip}>Skip</button>
+                </form>
+            </div>
         </div>
-
     )
 }
 
