f784be0b6475a41ec4b093ef591181e8a0d623dd
Mohsin Rashid <mohsinrashid32@gmail.com>
2022-05-24 19:20:37 +0500

    Login screen UI updated

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -42,7 +42,7 @@ const Login = ({ service, setAuth, skipAuth }) => {
             <div className='login-form'>
                 <h2>{(service.header) ? service.header : 'Please Login'}</h2>
                 <p>{(service.description) ? service.description : ''}</p>
-                <span class>{failureMsg}</span>
+                <span className="error-message">{failureMsg}</span>
                 <form>
                     <div className='mb-5'><input value={username} onChange={setEmailValue} type="text" placeholder="Enter email" className="px-4 pt-4 pb-4 border w-full rounded-md" /></div>
                     <div className='mb-5'><input value={password} onChange={setPassowrdValue} type="password" placeholder="password" className="px-4 pt-4 pb-4 border w-full rounded-md" /></div>
