219ea9892346337b7eb0a912b060b97a08fe9f37
Nouman Tayyab <noumantayyab@gmail.com>
2023-09-28 18:42:24 +0500

    version updated and installation

--- src/components/metadata/metadata.js
+++ src/components/metadata/metadata.js
@@ -26,7 +26,7 @@ const Metadata = (props) => {
     if (isFetching) return descLoader();
     if (dataError) return <span>Annotation structure is not correct</span>;
     if (!descriptionData) return <span>No metadata available.</span>;
-    
+
     return (
         <div className="">
             <div className="scroll custom-height overflow-x-hidden overflow-y-auto">
