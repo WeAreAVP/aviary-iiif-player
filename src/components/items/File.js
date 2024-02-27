75ba8bbdce6872eb4e37af47cbc4619d88803de4
Raza Saleem <razasaleem1991@gmail.com>
2024-02-27 16:50:23 +0500

    AVIARY-4408_AVIARY-4405_AVIARY-4402

--- src/components/items/File.js
+++ src/components/items/File.js
@@ -2,9 +2,10 @@ import React, { useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { setItem } from '../../features';
 import moment from 'moment'
+import thumbnail from '../../assets/images/audio-default.png';
 
 const File = (props) => {
-
+console.log('thumbnail',thumbnail)
     const dispatch = useDispatch();
     const [value, setValue] = useState(false)
 
@@ -20,7 +21,7 @@ const File = (props) => {
                     alt=''
                     style={{ cursor: 'pointer', objectFit: 'cover', borderRadius: '0.375rem' }}
                     className="cursor-pointer object-cover rounded-md"
-                    src={props.thumbnail ? props.thumbnail : "https://i.imghippo.com/files/alE651709026170.png"}
+                    src={props.thumbnail ? props.thumbnail : thumbnail}
                 />
                 <span style={{ position: 'absolute', right: '0.25rem', bottom: '0.25rem', fontWeight: 'bold', fontSize: '0.75rem', lineHeight: '1rem', background: 'black', color: 'white', paddingLeft: '0.25rem', paddingRight: '0.25rem', borderRadius: '0.25rem' }} className="absolute right-1 bottom-1 font-bold text-xs bg-black text-white px-1 rounded">{moment.utc(props.duration * 1000).format('HH:mm:ss')}</span>
             </div>
