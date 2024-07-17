b6281c2bc42be526fd9c0f8b84f9dfb5e186fd60
Raza Saleem <razasaleem1991@gmail.com>
2024-07-18 03:37:59 +0500

    AVIARY-4589_auth_service_2.0

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -5,40 +5,19 @@ const Login = ({ service, setAuth, skipAuth }) => {
     const [failureMsg, setFailureMsg] = useState("");
     const [newTab, setNewTab] = useState(null);
 
-    const handleTabClose = () => {
-        // Handle tab close event here
-        console.log('close',newTab)
-        console.log('service',service.service[0]['@id'])
-        
-    }
+   
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log('service',service)
+        console.log('service',service.service[0].id)
 
-        const tab = window.open(service['@id']+'?origin='+window.location.origin.toString(), '_blank');
+        const tab = window.open(service.service[0].id+'?origin='+window.location.origin.toString(), '_blank');
         setNewTab(tab);
         const checkTabClosed = setInterval(() => {
             if (tab.closed) {
               console.log('Tab closed');
               clearInterval(checkTabClosed); // Stop the interval
               setNewTab(null); // Reset the state
-              const iframe = document.createElement('iframe');
-              iframe.src = service.service[0]['@id']+'?origin='+window.location.origin.toString();
-              iframe.style.width = '600px';
-              iframe.style.height = '400px';
-              iframe.style.border = '1px solid black';
-              document.body.appendChild(iframe);
-              iframe.onload = () => {
-                // Access the iframe's document
-                const iframeDocument = iframe.contentWindow.document;
-          
-                // Get the content of the iframe
-                const content = iframeDocument.body.innerHTML;
-                setIframeContent(content);
-                console.log('content',content)
-                // Optionally, you can remove the iframe from the DOM
-                // document.body.removeChild(iframe);
-              };
+              axios.get(service.service[0].service[0].id+'?origin='+window.location.origin.toString(), { withCredentials: true });
             }
         }, 1000); // Check every second
         
